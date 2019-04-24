e34e695a7c1bf9e7b5a36a8fefe27678b95cd0cf
Faïssal <faissal@MacBook-Pro-de-Faissal.local>
2019-04-24 17:32:56 +0200

    add options associated to globe focus

--- src/components/Globe/utils/useFocus.js
+++ src/components/Globe/utils/useFocus.js
@@ -1,11 +1,19 @@
 import { useState } from 'react';
+import { markers } from './markers';
 
 export function useFocus() {
   const [focus, setFocus] = useState(undefined);
+  const focusButtonsDataset = ['None', ...markers.map(marker => marker.city)];
+  const focusOptions = {
+    enableDefocus: false
+  };
 
-  function goTo(coordinates) {
-    setFocus(coordinates);
+  function goTo(city) {
+    if (city === 'None') return setFocus(undefined);
+
+    const focus = markers.filter(marker => marker.city === city);
+    setFocus(focus[0]);
   }
 
-  return { focus, goTo };
+  return { focus, focusOptions, focusButtonsDataset, goTo };
 }
