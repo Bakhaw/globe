f58e02c8c789fe9cc90f6228e79d928227f326b4
Faïssal <faissal@MacBook-Pro-de-Faissal.local>
2019-04-24 17:32:38 +0200

    add cities in dataset

--- src/components/Globe/utils/markers.js
+++ src/components/Globe/utils/markers.js
@@ -5,6 +5,20 @@ const markers = [
     color: '#ccc',
     coordinates: [50.08804, 14.42076],
     value: 50
+  },
+  {
+    id: 2,
+    city: 'Paris',
+    color: '#ccc',
+    coordinates: [48.856614, 2.3522219],
+    value: 50
+  },
+  {
+    id: 3,
+    city: 'Colombo',
+    color: '#ccc',
+    coordinates: [6.927079, 79.861244],
+    value: 50
   }
 ];
 
